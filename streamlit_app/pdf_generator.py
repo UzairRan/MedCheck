d1b347d8fb7dccae30cb999f88e0a24112b13257
UzairRan <uzairrana183@gmail.com>
2025-08-08 11:39:11 -0700

    Switch to WeasyPrint for PDF generation on Streamlit Cloud

--- streamlit_app/pdf_generator.py
+++ streamlit_app/pdf_generator.py
@@ -1,20 +1,17 @@
 # streamlit_app/pdf_generator.py
-import pdfkit
 from jinja2 import Environment, FileSystemLoader
 import os
+from weasyprint import HTML
 
 def generate_pdf(data):
     # Load template
     template_dir = os.path.join(os.path.dirname(__file__), 'templates')
     env = Environment(loader=FileSystemLoader(template_dir))
     template = env.get_template('result.html')
-
+    
     # Render HTML
     html_out = template.render(**data)
 
-    # Use Streamlit Cloud path
-    config = pdfkit.configuration(wkhtmltopdf='/app/bin/wkhtmltopdf')
-
-    # Generate PDF in memory (return bytes)
-    pdf_data = pdfkit.from_string(html_out, False, configuration=config)
+    # Convert HTML to PDF using WeasyPrint
+    pdf_data = HTML(string=html_out).write_pdf()
     return pdf_data 
